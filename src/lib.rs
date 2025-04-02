5c09014a3eb69ec329c70353d2d46ad74574d4b5
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-04-02 13:46:47 +0300

    feat: add multiply fn

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,29 @@ pub fn add<T: Num>(left: T, right: T) -> T {
     left + right
 }
 
+pub fn multiply<T: Num>(left: T, right: T) -> T {
+    left * right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn add_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
 
         let result = add(2.0, 2.5);
         assert_eq!(result, 4.5);
+    }
+
+    #[test]
+    fn multiply_works() {
+        let result = multiply(2, 3);
+        assert_eq!(result, 6);
+
+        let result = multiply(2.0, 2.5);
+        assert_eq!(result, 5.0);
     }
 }
